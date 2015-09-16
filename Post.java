926522c97aab1f06234a08fd626f011b63bf0dd5
Stephanie Torres <sttorresji@unal.edu.co>
2015-09-16 14:48:58 -0500

    - Creación clase Post

---
+++ Post.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+/**
+ * Created by Stephanie on 16/09/2015.
+ */
+public class Post{
+    private String topic;
+    private Date dateCreated;
+    private Date lastUpdate;
+    private boolean isAllowed;
+    private ArrayList<File>files;
+    
+}
