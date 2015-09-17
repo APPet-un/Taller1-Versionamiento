ee8c0c788ed2f27b088e4a86786dc74280efe781
jhhfrancos <jhhfrancos@hotmail.com>
2015-09-16 23:37:41 -0500

    Add Forum and Admin Class

---
+++ Admin.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+/**
+ * Created by Jhh on 16/09/2015.
+ */
+public class Admin extends User{
+    private int level;
+    private double rating;
+    private ArrayList<Forum> forums;
+}
---
+++ Forum.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+/**
+ * Created by Jhh on 16/09/2015.
+ */
+public class Forum {
+    private String name;
+    private Date dateCreated;
+    private String category;
+    private Admin admin;
+}
