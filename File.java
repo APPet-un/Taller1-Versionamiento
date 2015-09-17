a5c971493c456109879b1564e8103a43c7139cdb
Sebastian <smunozca@unal.edu.co>
2015-09-16 22:30:52 -0500

    commit -Clase File actualizada con constructor y variables

--- File.java
+++ File.java
@@ -1,9 +1,24 @@
 /**
  * Author(s) Sebastian
  */
-public class File{
-
+public class File
+{
+	//Constructores
 	public File()
 	{
 	}
+	
+	public File (String fileType, Byte[] content, double size)
+	{
+		this.fileType= fileType;
+		this.content= content;
+		this.size= size;
+	}
+	
+	//Variables
+	private String fileType;
+	Byte[] content;
+	double size;
+	
+	
 }
