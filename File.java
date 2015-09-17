34fdafc49906e600be736756d63cc91caf229e82
Stephanie Torres <sttorresji@unal.edu.co>
2015-09-17 00:16:17 -0500

    - Adición de métodos download y share en la clase File

--- File.java
+++ File.java
@@ -3,7 +3,20 @@
  */
 public class File{
 
-	public File()
-	{
+	public File(){}
+	
+	public File(String fileType, Byte[] content, double size){
+		this.fileTye = fileType;
+		this.content = content;
+		this.size = size;
 	}
+	public void download(){
+		//TODO
+	}
+	public void share(){
+		//TODO
+	}
+	private String fileType;
+	private Byte[] content;
+	private double size;
 }
