23efbf46512351ef05d20a04d95a5d057a779570
Alvaro <aldrodriguezca@unal.edu.co>
2015-09-16 23:27:22 -0500

    Regular.java class w/ getters & setters

--- Regular.java
+++ Regular.java
@@ -8,6 +8,46 @@ public class Regular extends User{
     private User user;
     private ArrayList<Post> posts;
 
+     //Methods - getters & setters
+    public int getPostViews() {
+        return postViews;
+    }
+
+    public void setPostViews(int postViews) {
+        this.postViews = postViews;
+    }
+
+    public int getStrikesNumber() {
+        return strikesNumber;
+    }
+
+    public void setStrikesNumber(int strikesNumber) {
+        this.strikesNumber = strikesNumber;
+    }
+
+    public int getStarsNumner() {
+        return starsNumner;
+    }
+
+    public void setStarsNumner(int starsNumner) {
+        this.starsNumner = starsNumner;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public ArrayList<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(ArrayList<Post> posts) {
+        this.posts = posts;
+    }
 
 
 }
